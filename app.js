64bb5a958b3ce3aac992a832c357df7a5c07beba
Kevin <kevinsetiawan005@gmail.com>
2022-09-13 17:16:25 +0700

    submit javascript addEventListener

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("Hello World");
+let counter = 0;
+function count() {
+    counter++;
+    let h1 = document.querySelector('h1');
+    h1.innerHTML = counter;
+    if (counter%10 === 0) {
+        alert(`Count is now ${counter}`);
+    }
+
+}
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('button').onclick = count;
+    // document.querySelector('button').addEventListener('click',count);
+});
